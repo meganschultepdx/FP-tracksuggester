83f96158ae65069f8982d67277aa4425953e5196
megan schulte <megan.schulte.pdx@gmail.com>
2019-03-29 10:22:13 -0700

    add function and variables to scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event){
+
+    var ask1 = $("input:radio[name=ask1]:checked").val();
+    var ask2 = $("input:radio[name=ask2]:checked").val();
+    var ask3 = $("input:radio[name=ask3]:checked").val();
+    var ask4 = $("input:radio[name=ask4]:checked").val();
+    var ask5 = $("input:radio[name=ask5]:checked").val();
+
+    if ()
+  }
+}
